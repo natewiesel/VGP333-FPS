3b7cf4bcec4eb77106cb23594f66a07d4870b5e2
Nate Wiesel <natewiesel@gmail.com>
2016-08-01 00:06:38 -0700

    initial commit, just set up the test scene and Mouse Look script

---
+++ NateWiesel_FPS/Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonMovement : MonoBehaviour {
+
+    public float moveSpeed = 6.0f;
+    public float gravity = Physics.gravity.magnitude;
+
+    private CharacterController _char;
+
+	// Use this for initialization
+	void Start () {
+        _char = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ NateWiesel_FPS/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+    private float _rotX = 0;
+    private float _rotY = 0;
+    public float verticalLookLimit = 60.0f;
+
+    public float sensitivityHorizontal = 5f;
+    public float sensitivityVertical = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rbody = GetComponent<Rigidbody>();
+        if (rbody != null)
+        {
+            rbody.freezeRotation = true;
+        }
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        _rotX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
+        _rotX = Mathf.Clamp(_rotX, -verticalLookLimit, verticalLookLimit);
+
+        float d = Input.GetAxis("Mouse X") * sensitivityHorizontal;
+        _rotY = transform.localEulerAngles.y + d;
+        transform.localEulerAngles = new Vector3(_rotX, _rotY, 0);
+	}
+}
