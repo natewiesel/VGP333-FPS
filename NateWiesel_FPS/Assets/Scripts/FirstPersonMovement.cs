f0dfbcc2016598289652de5f362a1f103ba54e16
Nate Wiesel <natewiesel@gmail.com>
2016-08-01 17:20:20 -0700

    Imported standard character scripts

--- NateWiesel_FPS/Assets/Scripts/FirstPersonMovement.cs
+++ NateWiesel_FPS/Assets/Scripts/FirstPersonMovement.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class FirstPersonMovement : MonoBehaviour {
 
     public float moveSpeed = 6.0f;
-    public float gravity = Physics.gravity.magnitude;
+    private float gravity = -Physics.gravity.magnitude;
+    private float gravityPull = 0.5f;
+
+    private float Xspeed = 0f;
+    private float Zspeed = 0f;
 
     private CharacterController _char;
 
@@ -15,6 +19,23 @@ public class FirstPersonMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //Movement Input
+        Xspeed = Input.GetAxis("Horizontal") * moveSpeed;
+        Zspeed = Input.GetAxis("Vertical") * moveSpeed;
+        float jumpInput = Input.GetAxis("Jump");
+
+        Vector3 move = new Vector3(Xspeed, 0, Zspeed);
+        move = Vector3.ClampMagnitude(move, moveSpeed);
+
+        ////Gravity
+        //if (move.y > gravity)
+        //{
+        //    move.y -= gravityPull;
+        //}
+        ////move.y = gravity;
+
+        move *= Time.deltaTime;
+        move = transform.TransformDirection(move);
+        _char.Move(move);
 	}
 }
