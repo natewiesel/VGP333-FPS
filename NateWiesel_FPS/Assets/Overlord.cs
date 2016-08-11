d355c367e2f532704ced78f2952daafe43d77994
Nate Wiesel <natewiesel@gmail.com>
2016-08-10 22:03:54 -0700

    class 6 happened

---
+++ NateWiesel_FPS/Assets/Overlord.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Overlord : MonoBehaviour {
+
+    private static Overlord instance;
+    
+    public static  Overlord Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Overlord>();
+            }
+            return instance;
+        }
+    }
+
+
+    void ThingHappens()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        SendMessage("ThingHappens", SendMessageOptions.DontRequireReceiver);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
